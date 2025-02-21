6e3751667cc254deace33280c58defab544850e0
Luke Checchio <luke.checchio@duke.edu>
2025-02-20 19:04:45 -0500

    Experimenting

--- Script.R
+++ Script.R
@@ -8,4 +8,11 @@ library(ncaahoopR)
 ncaahoopR::get_roster("Duke")
 print(ids, n = 370)
 
-get_pbp("Duke")
+ids <- ids
+ncaa_colors <- ncaa_colors
+
+get_pbp(150)
+get_master_schedule("2025-02-20")
+
+gg_wp_chart(401724844, "#001A57", "#7BAFD4")
+duke_unc <- get_pbp_game(401724844)
